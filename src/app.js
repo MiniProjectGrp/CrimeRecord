aa0d8d83dfe07b781f26b92d1d6d63a1304e1d75
Vasu7777py <Subbannavarvasu@gmail.com>
2021-12-31 16:06:57 +0530

    added crime and criminal ejs files

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.use("/styles",express.static(__dirname + "/styles"));
+app.use("/styles", express.static(__dirname + "/styles"));
+app.use("/img" ,express.static(__dirname + "/images"));
 
 const PORT = 3000;
 
@@ -13,6 +14,10 @@ app.get("/", (req, res) => {
 
 app.get("/crime", (req, res) => {
     res.render("crime");
+});
+
+app.get("/criminal", (req, res) => {
+    res.render("criminal");
 });
 
 app.listen(PORT, () => {
